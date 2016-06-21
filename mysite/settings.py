6d0f380b4b6a27c615420780b092d00085ebef7a
lzq941129 <577142965@qq.com>
2016-06-21 16:25:17 +0800

    commit_4

--- mysite/settings.py
+++ mysite/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -71,7 +72,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
-
+ 
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
--- mysite/urls.py
+++ mysite/urls.py
@@ -13,13 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url,include
+from django.conf.urls import patterns,url,include
 from django.contrib import admin
 admin.autodiscover()
 
 from mysite.view import hello
 
 urlpatterns = [
+    url(r'^$','polls.views.home_page',name='home'),
     url(r'^admin/', admin.site.urls),
     url('^hello/$', hello),
     url(r'^polls/', include('polls.urls',namespace="polls")),
